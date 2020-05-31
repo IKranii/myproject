f883f3144cf39a0c42d8f1d6843c38b75134e8e7
IKranii <51850489+IKranii@users.noreply.github.com>
2020-05-31 14:19:33 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
                         </li>
                         <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            table
+                            Table
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a class="nav-link" href="http://localhost/myproject/public/teacher">Teacher<span class="sr-only">(current)</span></a>                
